1442c32bd5a60970d64996300bc2a25422bd28c5
Lulu <lulumutuli24@gmail.com>
2018-05-18 16:06:07 +0300

    basic structure of models on both apps added

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Destination(models.Model):
+    name= models.CharField(max_length=100)
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    num_plate = models.CharField(max_length=60)
+    num_of_seats= models.IntegerField(max_digits=8)
+
+class Pickup_Location(models.Model):
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+
 # Create your models here.
+class Driver(models.Model):
+    name = models.CharField(max_length=100)
+    car = models.Foreignkey
+    passenger_pickup = models.Foreignkey(Pickup_Location)
+    phone = models.IntegerField(max_digits=10)
+    destination = models.Foreignkey(Destination)
--- passenger/models.py
+++ passenger/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from driver import Destination, Pickup_Location
 # Create your models here.
+class Passenger(models.Model):
+    name= models.CharField(max_length=60)
+    phone = models.CharField(max_digits=10)
+    final_destination = models.Foreignkey(Destination)
+    pickup_point = models.Foreignkey(Pickup_Location)
