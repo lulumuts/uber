9f93ee8c7f4bbe41e8680999e9a7f14c2a2d904f
Lulu <lulumutuli24@gmail.com>
2018-05-23 14:02:30 +0300

    map working

--- driver/forms.py
+++ driver/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from leaflet.forms.fields import PointField
 from .models import Driver,Pickup_Location
+from leaflet.forms.widgets import LeafletWidget
+
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
@@ -10,3 +12,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
+
+
+class MapsForm(forms.ModelForm):
+
+    class Meta:
+        model = Pickup_Location
+        fields = ('name', 'location')
